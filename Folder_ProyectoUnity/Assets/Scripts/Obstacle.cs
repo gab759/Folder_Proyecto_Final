d8027fd2c210b96708c36930d0ebaec0082c552c
gab759 <150464325+gab759@users.noreply.github.com>
2024-11-26 02:28:15 -0500

    Implement modeling

--- Folder_ProyectoUnity/Assets/Scripts/Obstacle.cs
+++ Folder_ProyectoUnity/Assets/Scripts/Obstacle.cs
@@ -6,13 +6,25 @@ public class Obstacle : MonoBehaviour
 {
     public float speed = 5f;
 
+    void Start()
+    {
+        Vector3 adjustedPosition = transform.position;
+        adjustedPosition.y -= 0.684f;
+        transform.position = adjustedPosition;
+
+        transform.rotation = Quaternion.Euler(-90, 0, 0);
+    }
     void Update()
     {
-        transform.Translate(Vector3.back * speed * Time.deltaTime);
+        transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Delete"))
+        {
+            Destroy(gameObject);
+        }
+        if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
